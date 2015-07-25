a237586fc9f628fd7eb27471d41e5843060558d1
Immo Landwerth <immo@landwerth.net>
2015-07-25 16:00:38 -0700

    Extract parsers into fields
    Also, make sure that Int32 is parsed using the invariant culture.

--- src/System.CommandLine/CommandLineSyntax.Definers.cs
+++ src/System.CommandLine/CommandLineSyntax.Definers.cs
@@ -4,52 +4,56 @@ namespace System
 {
     public partial class CommandLineSyntax
     {
+        private static readonly Func<string, string> StringParser = v => v;
+        private static readonly Func<string, bool> BooleanParser = v => bool.Parse(v);
+        private static readonly Func<string, int> Int32Parser = v => int.Parse(v, CultureInfo.InvariantCulture);
+
         // Qualifiers
 
         public void DefineQualifier(string name, ref string value, string help)
         {
-            DefineQualifier(name, ref value, v => v, help);
+            DefineQualifier(name, ref value, StringParser, help);
         }
 
         public void DefineQualifier(string name, ref bool value, string help)
         {
-            DefineQualifier(name, ref value, bool.Parse, help);
+            DefineQualifier(name, ref value, BooleanParser, help);
         }
 
         public void DefineQualifier(string name, ref int value, string help)
         {
-            DefineQualifier(name, ref value, int.Parse, help);
+            DefineQualifier(name, ref value, Int32Parser, help);
         }
 
         // Qualifier arrays
 
         public void DefineQualifier(string name, ref string[] value, string help)
         {
-            DefineQualifier(name, ref value, v => v, help);
+            DefineQualifier(name, ref value, StringParser, help);
         }
 
         // Parameters
 
         public void DefineParameter(string name, ref string value, string help)
         {
-            DefineParameter(name, ref value, v => v, help);
+            DefineParameter(name, ref value, StringParser, help);
         }
 
         public void DefineParameter(string name, ref bool value, string help)
         {
-            DefineParameter(name, ref value, bool.Parse, help);
+            DefineParameter(name, ref value, BooleanParser, help);
         }
 
         public void DefineParameter(string name, ref int value, string help)
         {
-            DefineParameter(name, ref value, int.Parse, help);
+            DefineParameter(name, ref value, Int32Parser, help);
         }
 
         // Parameter arrays
 
         public void DefineParameter(string name, ref string[] value, string help)
         {
-            DefineParameter(name, ref value, v => v, help);
+            DefineParameter(name, ref value, StringParser, help);
         }
     }
 }

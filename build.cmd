db276b7fa101f625327ae673df9283179dac46fc
Immo Landwerth <immo@landwerth.net>
2015-07-25 14:45:35 -0700

    Replace individual parameters with wildcard
    It's not that we ever want to pass more than ten arguments; it's about
    the fact that we'd like to be able to pass arguments like this:
    
        .\build.cmd /p:OutDir='D:\test\'
    
    Using individual arguments, MSBuild is called like this:
    
        MSBuild.exe /p:OutDir D:\test\
    
    That's because the equal sign is considered a separator in batch files.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\build.proj /m /nologo /v:m /flp:verbosity=normal %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\build.proj /m /nologo /v:m /flp:verbosity=normal %*
